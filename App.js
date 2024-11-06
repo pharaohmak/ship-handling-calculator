8dfc4316a9cce981fb6266eb96fc21f0e3235116
Makram M. Ibrahim <61184060+pharaohmak@users.noreply.github.com>
2024-11-05 19:27:51 -0700

    ready for production -ios

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
@@ -16,7 +16,7 @@ import RatePerMin from './src/screens/RatePerMin';
 
 const Stack = createNativeStackNavigator();
 
-function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -25,79 +25,66 @@ function App() {
           component={HomeScreen}
           options={{ title: 'Ship Handling Calculator' }}
         />
-
         <Stack.Screen
           name="RadiusOfTurn"
           component={RadiusOfTurn}
           options={{ title: 'Calculate Radius of Turn' }}
         />
-
         <Stack.Screen
           name="RateOfTurnSpeed"
           component={RateOfTurnSpeed}
           options={{ title: 'Calculate ROT Speed' }}
         />
-
         <Stack.Screen
           name="RateOfTurn"
           component={RateOfTurn}
           options={{ title: 'Calculate Rate Of Turn' }}
         />
-
         <Stack.Screen
           name="AdvanceInTurn"
           component={AdvanceInTurn}
           options={{ title: 'Calculate Advance Distance' }}
         />
-
         <Stack.Screen
           name="VectorWaypoint"
           component={VectorWaypoint}
           options={{ title: 'Calculate Vector Length to Waypoint' }}
         />
-
         <Stack.Screen
           name="Distance"
           component={Distance}
           options={{ title: 'Calculate Distance' }}
         />
-
         <Stack.Screen
           name="Speed"
           component={Speed}
           options={{ title: 'Calculate Speed' }}
         />
-
         <Stack.Screen
           name="Time"
           component={Time}
           options={{ title: 'Calculate Time' }}
         />
-
         <Stack.Screen
           name="SwingCircle"
           component={SwingCircle}
           options={{ title: 'Calculate Anchor Swing Circle' }}
         />
-
         <Stack.Screen
           name="RatePerMin"
           component={RatePerMin}
-          options={{ title: 'Calculate Roataions Per Minute' }}
+          options={{ title: 'Calculate Rotations Per Minute' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
-
-
+};
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-
 });
 
 export default App;
